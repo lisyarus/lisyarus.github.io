a82f1fc69d241b00f4ab4d61a1aa0ab27c4c7c9a
lisyarus <lisyarus@gmail.com>
2018-02-09 16:09:32 +0300

    Redesign index.html

--- update-index.py
+++ update-index.py
@@ -5,12 +5,14 @@ import os
 
 
 def main():
-	
-
 
 	template = None
 	with open('index.html.template', 'rt') as f:
 		template = f.read()
+
+	element_template = None
+	with open('element.html.template', 'rt') as f:
+		element_template = f.read()
 
 	link_lines = []
 
@@ -18,14 +20,14 @@ def main():
 		for file in files:
 			if file != 'index.html' and file.endswith('.html'):
 				link = file
-				name = link[0].upper() + link[1:-5].replace('_', ' ')
-				image_link = 'screenshots/' + link[:-5] + '.png'
-				line = "<tr><td width='30%'><a href='" + link + "'>" + name + "</a></td><td><img width='100%' src='" + image_link + "'></td></tr>"
+				name = link[:-5]
+				rname = name[0].upper() + name[1:].replace('_', ' ')
+				line = element_template.replace('$NAME', name).replace('$RNAME', rname)
 				link_lines.append(line)
 
 		
-	links_html = '<br/>\n'.join(link_lines)
-	output_html = template.replace('$LINKS', links_html)
+	links_html = '\n'.join(link_lines)
+	output_html = template.replace('$ENTRIES', links_html)
 	
 	with open('index.html', 'wt') as f:
 		f.write(output_html)
